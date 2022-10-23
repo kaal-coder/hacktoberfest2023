bedde4cc9681f8833f5a3adbe605a0f31b9b9a15
PadamMantry04 <ui21ec46@iiitsurat.ac.in>
2022-10-23 23:30:09 +0530

    CODECHEF "Insurance"
    CODECHEF "Insurance" solution provided

---
+++ Coding/INSURANCE.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x=0;
+        int y=0;
+        cin>>x>>y;
+        if(x>=y){
+            cout<<y<<endl;
+        }
+        else{
+            cout<<x<<endl;
+        }
+    }
+}
