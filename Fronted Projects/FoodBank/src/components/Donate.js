6ee9181002afa9d6886049c487aba33fa9e0b2c3
Rahul_oops <rahuldas20042000@gmail.com>
2022-10-25 23:18:19 +0530

    Foodbank Frontend project

---
+++ Fronted Projects/FoodBank/src/components/Donate.js
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import Create from '../modal/Create'
+import Card from './Card';
+
+const Donate = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+    
+    useEffect(() => {
+        let arr = localStorage.getItem("taskList")
+       
+        if(arr){
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
+
+
+    return (
+        <>
+            <div className = "header text-center">
+                <h3>Food Donation App</h3>
+                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >donate Food</button>
+            </div>
+            <div className = "task-container">
+            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
+            </div>
+            <Create toggle = {toggle} modal = {modal} save = {saveTask}/>
+        </>
+    );
+};
+
+export default Donate;
