d22dfde5df34b873040494f326daff6e141f9160
ANKUSH JAIN <133674486+AnkushjainAj@users.noreply.github.com>
2023-10-10 23:20:25 +0530

    Update Maximum_subArray.java
    This is for issue #161 
    - using Kadane's algorithm

--- Coding/Maximum_subArray.java
+++ Coding/Maximum_subArray.java
@@ -10,29 +10,28 @@ Example 3:
 	Input: nums = [5,4,-1,7,8]
 	Output: 23
 */
-public class Maximum_subArray {
+public class MaximumSubarraySum {
 
-	static public int maxSubArray(int[] nums) 
-    {
-        int sum = 0;
-        int max = nums[0];
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
 
-        int i= 0;
-        while(i<nums.length)   
-        {
-        	sum += nums[i];
-            max = Math.max(max,sum);
-            if(sum<0) sum= 0;
-            i++;
+        // Loop through the array starting from the second element
+        for (int i = 1; i < nums.length; i++) {
+            // Calculate the maximum between the current element and the current element + currentSum
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update the maxSum if the currentSum is greater
+            maxSum = Math.max(maxSum, currentSum);
         }
-        return max;
+
+        return maxSum;
     }
-	
-	public static void main(String[] args) 
-	{
-		int arr [] = {-2,1,-3,4,-1,2,1,-5,4}; // change array for different inputs
-		System.out.println( maxSubArray(arr) );
 
-	}
-
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSubArraySum = maxSubArray(nums);
+        System.out.println("Maximum Subarray Sum: " + maxSubArraySum);
+    }
 }
+
