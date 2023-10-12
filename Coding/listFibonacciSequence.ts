44913ff305aba14d498c44b4f6faa433e30d639b
czarjulius <juliusczar.jc@gmail.com>
2023-10-12 12:42:26 +0100

    fix typo

--- Coding/listFibonacciSequence.ts
+++ Coding/listFibonacciSequence.ts
@@ -1,4 +1,4 @@
-const listFibonnachiSequence = (n: number): number[] => {
+const listFibonacciSequence = (n: number): number[] => {
   let output: number[] = [0, 1];
   for (let i = 2; i <= n; i++) {
     const nextValue: number = output[i - 1] + output[i - 2];
