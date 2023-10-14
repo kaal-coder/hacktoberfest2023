09fcd63add45c3c1985ea3c8668e81be77b17431
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-10-14 17:09:55 +0530

    Created new folders for each programming languageand organised all the files in their respective folders

--- Coding/SOL1.java
+++
@@ -1,9 +0,0 @@
-public class SOL1 {
-    public static void main(String[] args) {
-        int x;
-        int y;
-        x=2;
-        y=2;
-        System.out.println("The Answer is = "+ x*y);
-    }
-}
