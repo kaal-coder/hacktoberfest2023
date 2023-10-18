061395db85b4fea55830fa930c557e6f7bea91ec
hackgone <bishtpiyush17@gmail.com>
2023-10-18 21:15:52 +0530

    new program in python folder

---
+++ Python Projects/sudukoSolver/game.py
@@ -0,0 +1,60 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def is_valid(board, row, col, num):
+    # Check if the number is not in the current row and column
+    if num in board[row] or num in [board[i][col] for i in range(9)]:
+        return False
+    
+    # Check if the number is not in the current 3x3 grid
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    empty_loc = find_empty_location(board)
+    if not empty_loc:
+        return True  # Sudoku solved successfully
+    row, col = empty_loc
+    
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0  # Undo the move if it leads to an incorrect solution
+    return False
+
+# Example Sudoku puzzle (0 represents empty cells)
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku puzzle:")
+print_board(sudoku_board)
+print("\nSolving...\n")
+
+if solve_sudoku(sudoku_board):
+    print("Sudoku solved:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists.")
